5a23fa3fe500918abbecda170c64da59bc92a524
S. M. Hasibur Rahman <hasib.jhd@gmail.com>
2020-07-06 14:10:08 +0600

    [Calculator] Action listener successfully added
    [Details]
    
    [Developer] S. M. Hasibur Rahman
    [Type] Feature Implementation

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calculatoractivity_main);
+        final TextView displayText = findViewById(R.id.calulatorScreen);
+        Button n0 = findViewById(R.id.n0);
+        Button n1 = findViewById(R.id.n1);
+        Button n2 = findViewById(R.id.n2);
+        Button n3 = findViewById(R.id.n3);
+        Button n4 = findViewById(R.id.n4);
+        Button n5 = findViewById(R.id.n5);
+        Button n6 = findViewById(R.id.n6);
+        Button n7 = findViewById(R.id.n7);
+        Button n8 = findViewById(R.id.n8);
+        Button n9 = findViewById(R.id.n9);
+
+        View.OnClickListener calculatorButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayText.setText("hasin");
+            }
+        };
+        n0.setOnClickListener(calculatorButtonListener);
     }
 }
